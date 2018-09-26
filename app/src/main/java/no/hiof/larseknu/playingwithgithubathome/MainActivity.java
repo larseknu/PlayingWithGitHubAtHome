320c930868862c8c040ff41bc717018e8fe612dd
Lars Emil Knudsen <larsemil87@gmail.com>
2018-09-26 10:20:46 +0200

    Button handling in MainActivity

--- app/src/main/java/no/hiof/larseknu/playingwithgithubathome/MainActivity.java
+++ app/src/main/java/no/hiof/larseknu/playingwithgithubathome/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button goToMovieButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToMovieButton = findViewById(R.id.goToMovie);
     }
 
     public void goToMovies(View view) {
